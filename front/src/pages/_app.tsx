9c739f79715a22730d9c2e986c0422c6645f9313
Cristhian Melo <rxtsel@hotmail.com>
2023-04-23 14:55:51 -0500

    chore(front): add title and fonts

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,37 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Urbanist, Oswald } from 'next/font/google'
+import { Header, Footer } from '@/components'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const urbanist = Urbanist({
+  subsets: ['latin']
+})
+
+const oswald = Oswald({
+  subsets: ['latin']
+})
+
+export default function App ({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>
+        {`
+        :root {
+          --font-urbanist: ${urbanist.style.fontFamily};
+          --font-oswald: ${oswald.style.fontFamily};
+        }
+      `}
+      </style>
+
+      <Head>
+        <meta charSet='utf-8' />
+        <title>rxtsel - Tienda Virtual</title>
+      </Head>
+
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
