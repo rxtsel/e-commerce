a2d2decbe4edfb03d602666831852123ad158e70
Cristhian Melo <rxtsel@hotmail.com>
2023-04-30 17:25:25 -0500

    fix(front): bug fix build categories

--- front/src/pages/category/[slug].js
+++ front/src/pages/category/[slug].js
@@ -12,7 +12,7 @@ export default function Category () {
       <Wrapper>
         <div className='text-center max-w-[800px] mx-auto md:mt-0'>
           <h1
-            className='text-[28px] md:text-[34px] mb-5 font-semibold leading-tight'
+            className='text-[28px] md:text-[34px] mb-5 font-semibold leading-tight capitalize'
           >
             {title}
           </h1>
--- front/src/utils/index.js
+++ front/src/utils/index.js
@@ -1,5 +1,5 @@
 export const slugToTitle = slug => {
   return String(slug).includes('-')
-    ? slug.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-    : slug.charAt(0).toUpperCase() + slug.slice(1)
+    ? slug.split('-').join(' ')
+    : slug
 }
